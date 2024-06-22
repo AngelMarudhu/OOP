d9140738bbe6b41bf5813a097c197cd82686ad18
Marudhupandiyan <angeldeveloper09@gmail.com>
2024-06-22 22:36:15 +0530

    Conclusion + Generics

---
+++ src/Generics + conclusion/Exception.js
@@ -0,0 +1,84 @@
+"use strict";
+// interface Gen {
+//   length: number;
+// }
+// function logging<T extends Gen>(arg: T) {
+//   console.log(arg.length);
+// }
+// let result = logging([1, 2, 3, 4]);
+// console.log(result);
+// function getValues<T>(arg: T): void {
+//   if (Array.isArray(arg)) {
+//     for (let value of arg) {
+//       console.log(value);
+//     }
+//   } else if (typeof arg === "object") {
+//     for (let key in arg) {
+//       console.log(key);
+//     }
+//   }
+// }
+// let obj = { names: "Marudhu", age: 25, living: "coder" };
+// let out = getValues(obj);
+// let arr = [1, 2, 3];
+// // getValues(arr);
+/////////////////////////////// ROBOT ANALOGY \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// function moveRobot(distance: number): void {
+//   if (distance < 0) {
+//     throw new Error("something wrong");
+//   }
+//   console.log("hey please move forward");
+// }
+// try {
+//   moveRobot(-2);
+// } catch (error) {
+//   //// type guards helping to detection which one to execute during the run time that means actual object creation if you don't know go back to previous post
+//   if (error instanceof Error) {
+//     console.log(error.message);
+//   }
+// }
+// /// What is exception? very simple guys exception means problem will arise during the execution our code lines for example when the problem occurs it disrupt the normal flow of the codes isn't hahaha
+//// as i told you before don't compare with any other language okay in typescript offers only run time error deduction not possible to getting compile time error detection always run time we only getting an error when code execute okay
+/// 1) when error occurs in typescript we've creating a error object which means error class instantiation
+//// 2) then throwing the error along with flag messages like "Something is wrong check the code blah blah"
+//// 3) then catching an error here is the best thing we are using special block of code called try-catch block it will handle all the error smoothly
+//// 4) One scenario guys for example we are building a ROBOT there is some common normal instruction move forward, left, right, turn something our command hey robot move forward but there is a wall front of the robot that means problem occurs the robot exception internal system just detect the wall(creates error object) creating as error and throwing to us here front of me have a wall oh okay we gave a next instruction turn please so robot decide to turn and avoid the wall (handle error)
+// Exception: A problem that occurs when running a program
+// Throw: When the program stops and signals that something went wrong creating an error object with message what you want to say
+// Catch: When you handle the problem and decide what to do next
+//  Exception Handling
+//     Unchecked Exceptions (only run time detection means code execution stage)
+//         Error Classes (Instantiation)
+//             Error (actual object if you don't the terms please go back to previous posts)
+//                 Properties (states, instance properties)
+//                    name
+//                    message(string)
+//                   stack
+//                 Methods (function, instance methods)
+//                    toString()
+//         EvalError (class)
+//             Thrown when the eval() function is used improperly.
+//         RangeError (class)
+//             Thrown when a number is outside an allowable range.
+//         ReferenceError (class)
+//            Thrown when referencing a non-existent variable.
+//         SyntaxError (class)
+//             Thrown when there is a syntax error in code.
+//         TypeError (class)
+//             Thrown when a value is not of the expected type.
+//         URIError (class)
+//              Thrown when there is an error in URI handling functions.
+/// you can also create your own custom error subclasses
+// class CustomError extends Error {
+//   constructor(message: string) {
+//     super(message);
+//     this.name = "CustomError";
+//   }
+// }
+// try {
+//   throw new CustomError("This is a custom error.");
+// } catch (error) {
+//   if (error instanceof CustomError) {
+//     console.log(`Caught a custom error: ${error.message}`);
+//   }
+// }
---
+++ src/Generics + conclusion/Generics.ts
@@ -0,0 +1,179 @@
+//// GENERICS SHORT AND SWEET EXPLANATION
+
+// when we write TYPESCRIPT code that we have variety of data types to mention explicitly isn't for type safety right marudhu what does generic here GENERIC provides way to create reusable components & functions & classes & interface generics bundled up with single source which means you don't need to explicit type safety data types generics will take care of everything you just know where do we use and how do we use let's see GENERICS can be used : Class instance members, Class Methods, Function argument value and Funtion return types with sinlge source
+
+/////////////////////////////// USE CASES OF GENERICS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// 1) Reusabiity --> we can write a code once with different data types with help of GENERICS
+// 2) Type Safety ---> i hop you all know what it is ensuring the data type during the compile-time if anything goes wrong
+// 3) SingleSource---> you don't need to mention the data types repetedly GEN adopt based on user type used hahahahahah
+
+//// T ---> Before initial ----> T stands for any type and it could be any data type num, str.....
+//// T ---> After initial ----> When we call the function the T adopt based on the input
+
+////// MOST USED PLACES
+// Class instance members
+// Class methods
+// Function Arguments
+// Function return types
+
+/// function argument and function return automatically generic adopted you don't need to use any explicit data types
+
+/// type -> T capturing the data types the user provided eg string we can access that information later and there is no lose information
+
+function generic<T>(arg: T): T {
+  return arg;
+}
+
+// let out = generic("Marudhupandiyan");
+
+// let lengths = out.length;
+// console.log(lengths);
+
+// out = 44; //// compile error Type 'number' is not assignable to type 'string' this means once you've assigned value to the argument you can't change the data types the generic protected your datas
+
+//// you can also use this
+
+//// //// you may say we can use "any" yes you're correct but there is chances to leakage our data and "any" can lead to data leakage loss of type information and only gives runtime errors because it bypasses TypeScript's type checking ---> what is type checking before executing the code we need to address the error which compile time but any gives run time error for us
+
+function genericsss(arg: any): any {
+  return arg;
+}
+
+// let out = genericsss("Marudhu");
+
+// let lengths = out.length;
+
+// out = 33; /// no error you can assign any types no error
+// let another = out.length //// you get an run time error "undefined" ahahahha
+
+//////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+///// deep dive bit more about generics
+
+//// this function generics<T>(arg: T[]): T[]{}) can take an array OF any type and return an array of that same type this generics function can handle all the array of data type for example array of number, string or other types
+
+////// normal generic function
+
+function generics<T>(arg: T[]): T[] {
+  ///// before initialization we don't know what data type user gave us till call the function that's why we are getting an error //// Property 'length' does not exist on type 'T'
+  //// and nowhere we mentioned arg T has this member of the function argument we don't know sometime user can have provide number data types that time will rise a confusion number doesn't have length property by nature that's why before initialization getting error
+  console.log(arg.length);
+
+  return arg;
+}
+
+///////// If you want to handle a single string (or any type that has a length property) using generics
+//// there is bunch of solution for this for example you can mention explicity the data type besides the T argument like
+
+interface Names {
+  data: number | number[];
+  name: string;
+  age?: number;
+}
+
+interface Lengths {
+  length: number;
+}
+
+//// interface means you all well know what it is it's common for all of them and must implement all the properties you never escapes only compile time checking no run time checking during the compile time check the types and structures all
+
+///// generic constraints which means we can also access some other data types as well not only T data types like interface structure
+
+function genericss<T extends Names, length extends Lengths>(
+  arg: T,
+  lengthVal: length
+): T {
+  //   console.log(arg.length);
+  //   console.log(arg.name);
+  console.log(lengthVal.length, "names length" + " " + lengthVal);
+  return arg;
+}
+
+//// we must follow the structure with object why because it's easy to find the missing types or values mismatching types for example if we passed only data if we didn't pass the name while the time we getting an error all these happens in compile time we've already talked about no run time interface detection object structure will helps us and code clarity good type safe done
+
+let out = genericss({ data: 12, name: "Marudhupandiyan" }, "Nayanthara");
+
+// let outs = generics(123);
+//// you'll getting compile time error Argument of type 'number' is not assignable to parameter of type 'Lengths' because by nature number doesn't have length property that's why we getting error
+// by nature no possible to use length property for number data types
+// let data = 123;
+// data.length; /// Property 'length' does not exist on type 'number
+
+let out2 = genericss({ data: [1, 2], name: "Madonna" }, "Madonna sebastian");
+console.log(out2.name); //// it's works no error
+console.log(out.name); //// it's works no error
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+function greet(name: Names): void {
+  console.log(name.age);
+  console.log(name.name);
+}
+
+let done = greet({ name: "marudhu", data: 22, age: 24 });
+
+//////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//// in a usual method we can pass all the data types into array but we can easily seprerate them with simple generics helps generic classes
+
+class Gens<T, U> {
+  private data: T[];
+  private str: U[];
+  constructor() {
+    this.data = [];
+    this.str = [];
+  }
+
+  Push(item: T | U) {
+    if (typeof item === "number") {
+      ///// type assertion just "as" tells to typescript you're certain right data type so just push it this will resolve the confusion about checks
+      ///// we can't use instance of type guards here when we dealing with class instances this will helps us we cannot directly use primitive data types cause they are not classes
+      this.data.push(item as T);
+    } else {
+      this.str.push(item as U);
+    }
+  }
+
+  Pop() {
+    this.data.pop();
+  }
+  gettingData(): T[] | U[] {
+    return this.data;
+  }
+}
+
+let check = new Gens<number, string>();
+check.Push(12);
+check.Push(14);
+check.Push("Marudhu");
+check.Push("Nayanthara");
+
+console.log(check.gettingData());
+
+//// when we extend the class or creating subclasses inherit all the properties and methods and one more thing we are going to inherit that is generics that will allowing to reuse and extend the behavior of Gens with specific or generic types as needed
+
+class SubGen<T, U> extends Gens<T, U> {
+  constructor() {
+    super();
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+///// GENERIC FUNCTION WITH IN ARRAY
+
+function reverse<T>(array: T[]): T[] {
+  let returns = [];
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    returns.push(array[i]);
+  }
+  return returns;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+let reversedNumbers = reverse(numbers);
+console.log(reversedNumbers); // Output: [5, 4, 3, 2, 1]
+
+// reversedNumbers[0] = "Marudhu"; ///// compile time error Type 'string' is not assignable to type 'number'
+
+/// type erasure type erasure in TypeScript ensures that your code is type-safe at compile time while producing efficient and compatible JavaScript at runtime code or bytecode
