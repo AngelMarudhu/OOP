ec761ca498f0a0035b622fb9d424a7821b614ecc
Marudhupandiyan <angeldeveloper09@gmail.com>
2024-06-26 19:25:33 +0530

    structuring

--- src/AbstractClass/Vehicle.ts
+++ src/AbstractClass/Vehicle.ts
@@ -2,7 +2,7 @@
 
 //// still we can't make multiple inheritance not allowed we can't extend two abstract class into single derived or subclass as we know it's not possible buddy here it comes to help INTERFACE
 
-//// first thing whatever we've create the methods in abstract class we've must override all the methods in the child class or subclass isn't that's very first thing guys we've already covered what's overridden if you haven't check it out just go back to the polymorphism post done :)
+//// first thing whatever we've create the methods in abstract class we've must override all the methods in the child or derived class or subclass isn't that's very first thing guys we've already covered what's overridden if you haven't check it out just go back to the polymorphism post done :)
 
 export abstract class Vehicle {
   //// abstract method always be nudity we can't add any body for abstract methods it's just giving information to child classes in simple there is no more curly braces done
@@ -16,7 +16,9 @@ export abstract class Vehicle {
   }
   //// abstract methods crate for overridden
   //// static method behavior is can't overridden not possible to create static method but you can implement it
+
   //// we can't make static method overridden in abstract class what abstract class it's just overridden of subclass providing the methods ot subclass must implementation
+
   //// but as we seen we can't overridden the static method isn't the how it's possible buddy not possible hahaha static is behalf of class constructor buddy's
   // static Common(): void;
 
@@ -39,3 +41,12 @@ export abstract class Vehicle {
 //// Cannot create an instance or object of an abstract class not possible it's just method holder that's all
 
 // let out = new Vehicle();
+
+////// FINAL OUTPUT FOR REVISION IN SIMPLE PHRASES
+
+/////// We must override all the methods in the child or derived class
+////// Abstract methods must always be declared without a body
+////// If a class contains at least one abstract method the class itself must be declared as abstract
+///// Abs methods are created to be overridden
+///// Static methods cannot be overridden
+///// Cannot create an instance or object of an abstract class no curly and no body
